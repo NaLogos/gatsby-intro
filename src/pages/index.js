0accbfb98bc906e42008c537e6a9a6f03054bd5e
Zakarya <zakaryaaffani35@gmail.com>
2021-07-20 18:11:18 +0100

    Done with blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
 
-export default () => (
-  <>
-    <h1>Home</h1>
-    <p>Hello there</p>
-    <Link to="/about/">learn about me &rarr;</Link>
-  </>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello there</p>
+      <Link to="/about/">learn about me &rarr;</Link>
+
+      <h2>Read My Blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
